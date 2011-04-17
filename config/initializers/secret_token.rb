83d55fef0192b6223caad048b445070f4e0ee4a4
gspeager <gbits.dev@gmail.com>
2011-04-17 01:59:49 -0600

    First Commit
    - Posts edit, update, view
    - Comments add, remove
    - Tags add, remove

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '7b0cdb7934970fce844fe18eda371b8ec1a9d3e3f7c1397ccaef5d14f968ae563212d14068826bc1193714ad74b9584f1d51df8493d3713b8c9bdfcf97dbf0b6'
